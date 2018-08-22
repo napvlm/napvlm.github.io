b44c83fd8071c6a6dba32e37e87687252e8ab6c6
Ruslan <onlylucknplm@gmail.com>
2018-08-22 12:47:13 +0200

    adding new SM effects

--- projects/event-promo-landing/main.js
+++ projects/event-promo-landing/main.js
@@ -44,17 +44,43 @@ $(document).ready(function(){
 
  // Init ScrollMagic 
 	var controller = new ScrollMagic.Controller();
-	// build a scene
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#appear'
+	//about section
+	$('.scrollfade').each(function(){
+	var myScene = new ScrollMagic.Scene({
+		triggerElement: '.scrollfade',
+		reverse: false
 	})
-	.setClassToggle('#appear', 'fade-in')
-	// .addIndicators({
-	// 	name: 'fade scene',
-	// 	colorTrigger: 'black',
-	// 	indent:200,
-	// 	colorStart: '#75C695'
-	// })
+	.setClassToggle('.scrollfade', 'fade-in')
 	.addTo(controller); 
+	});
+	//callouts
+	$('.callout').each(function(){
+	var myScene = new ScrollMagic.Scene({
+		triggerElement: '.callout',
+		reverse: false
+	})
+	.setClassToggle('.callout', 'fade-in')
+	.addTo(controller); 
+	});
+
+	$('.callout2').each(function(){
+	var myScene = new ScrollMagic.Scene({
+		triggerElement: '.callout2',
+		reverse: false
+	})
+	.setClassToggle('.callout2', 'fade-in')
+	.addTo(controller); 
+	});
+
+	$('.callout3').each(function(){
+	var myScene = new ScrollMagic.Scene({
+		triggerElement: '.callout3',
+		reverse: false
+	})
+	.setClassToggle('.callout3', 'fade-in')
+	.addTo(controller); 
+	});
 
 });
+
+//End Scrollmagic effects
