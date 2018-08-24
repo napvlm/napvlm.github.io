d8b76b1ae89f20f72eb30f01a80e2502e8bdb132
Ruslan <onlylucknplm@gmail.com>
2018-08-24 20:58:58 +0200

    fixing scroll bugsl

--- projects/filanova-design/script.js
+++ projects/filanova-design/script.js
@@ -1,11 +1,11 @@
 //to make sure that header will takes up all the screen
 
 $(document).ready(function(){
- $('.header').height($(window).height());
+ $('.bcg').height($(window).height());
 });
 
 
-// scroll 
+// scroll to anchor
 $(".navbar a").click(function(){
 	$("body,html").animate({
 		scrollTop:$("#" +
@@ -16,3 +16,21 @@ $(this).data('value')).offset().top
 
 //Scrollspy
 $('body').scrollspy({target: ".navbar"})
+
+//ScrollMagic magic begin here :D 
+// Just a simple parallax
+
+$(document).ready(function(){
+
+	//init Scrollmagic
+
+	var controller = new ScrollMagic.Controller();
+
+	var parallaxScene = new ScrollMagic.Scene({
+		triggerElement: '.parallax',
+		trigerHook: 1,
+		duration: '200%'
+	})
+	.setTween(TweenMax.from('.bcg',1 , {y: '-30%', ease:Power0.easeNone}))
+	.addTo(controller);
+});
