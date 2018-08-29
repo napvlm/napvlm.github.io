e3a801d4b4461742a6e4b854d80e56c3deaca7c7
Ruslan <onlylucknplm@gmail.com>
2018-08-29 10:41:26 +0200

    edit lookbook page

--- projects/store-concept/js/lookbook.js
+++ projects/store-concept/js/lookbook.js
@@ -1,29 +1,30 @@
-//GSAP
-(function($){
-	var logowrap = $('.logowrap'),
-		list = $('.list');
 
-	TweenLite.from(logowrap, 1, {x:-200, ease:Power3.easeOut});
-	TweenLite.from(logowrap, 1, {autoAlpha:0, delay: 0.3});
-
-	TweenLite.from(list, 1, {y:200, ease:Power3.easeOut});
-	TweenLite.from(list, 1, {autoAlpha:0, delay: 0.3});
-})(jQuery);
-
-//ScrollMagic
 
 $(document).ready(function(){
+
+	//GSAP
+	var navbar = $('.navbar'),
+		content = $('#main');
+
+	TweenLite.from(navbar, 1, {x:-500, ease:Power3.easeOut});
+	TweenLite.from(navbar, 1, {autoAlpha:0, delay: 0.3});
+
+	TweenLite.from(content, 1, {y:500, ease:Power3.easeOut});
+	TweenLite.from(content, 1, {autoAlpha:0, delay: 0.3});
+
+
+     //Scrollmagic
 	var cntrl = new ScrollMagic.Controller();
 
 	var horizontalMoveTL = new TimelineMax();
 
 	horizontalMoveTL
-		.to('.horizontal-container', 1, {x:'-90%',ease:Linear.easeNone});
+		.to('.horizontal-container', 1, {x:'-66.6666%',ease:Linear.easeNone});
 
 	var pinMainScene = new ScrollMagic.Scene({
 		triggerElement: '#main',
 		triggerHook: 0,
-		duration: '1000%' 
+		duration: '300%' 
 	})
 	.setTween(horizontalMoveTL)
 	.setPin('#main')
