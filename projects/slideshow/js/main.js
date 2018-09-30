de66c145598697cdb9383d6ab24086ac022fae89
Napvlm <hello@ruslananisimov.com>
2018-09-30 21:06:49 +0200

    keep doing stuff

---
+++ projects/slideshow/js/main.js
@@ -0,0 +1,10 @@
+// (function ($) {
+
+	// Variables
+	var projects = document.querySellectorAll(.projects), // projects container
+		project = document.querySellectorAll(.project), // individual projects
+		projectImageBefore = ,
+		projectImageAfter = ,
+		tlProjects, tlProject;
+
+// })(jQuery);
