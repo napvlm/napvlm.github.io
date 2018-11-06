e1b93af81f3f159bee9f293b69214cc9110eeeb2
Napvlm <hello@ruslananisimov.com>
2018-11-06 12:24:45 +0200

    hz

--- x-girl/js/main.js
+++ x-girl/js/main.js
@@ -1,6 +1,7 @@
 
 $(window).on('load', function() { // makes sure the whole site is loaded 
   jQuery('#loader-wrapper').fadeOut("slow"); // will first fade out the loading animation 
+  $('body').delay(350).css({'overflow':'visible'});
 })
 
 $('.home_slider .owl-carousel, .about_us_slider .owl-carousel, .contacts_slider .owl-carousel').owlCarousel({
